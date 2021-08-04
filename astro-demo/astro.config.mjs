9ce4e4b518a55fbaa97e95ac478290882fc978a0
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2021-08-03 22:43:40 -0300

    mudança de template do astro.

--- astro-demo/astro.config.mjs
+++ astro-demo/astro.config.mjs
@@ -4,15 +4,12 @@ export default {
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,         // Generate sitemap (set to "false" to disable)
+    // site: 'http://example.com',  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    // sitemap: true,      // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on. 
-    // port: 3000,             // The port to run the dev server on.
-    // tailwindConfig: '',     // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
+    // port: 3000,         // The port to run the dev server on.
+    // tailwindConfig: '', // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
   },
-  renderers: [
-    "@astrojs/renderer-vue"
-  ],
+  renderers: ['@astrojs/renderer-preact'],
 };
